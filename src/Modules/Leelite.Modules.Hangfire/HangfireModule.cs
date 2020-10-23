9b140bc8d405e596e38309064ed75303674b98b4
liduopie <liduopie@163.com>
2020-10-23 16:36:57 +0800

    添加 Hangfire项目

---
+++ src/Modules/Leelite.Modules.Hangfire/HangfireModule.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Hangfire;
+using Hangfire.PostgreSql;
+
+using Leelite.AspNetCore.Modular;
+using Leelite.Commons.Host;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Leelite.Modules.Hangfire
+{
+    public class HangfireModule : MvcModuleStartupBase
+    {
+        public override void ConfigureServices(HostContext context)
+        {
+            var services = context.ServiceDescriptors;
+
+            var config = context.HostServices.GetService<IConfiguration>();
+
+            // Add Hangfire services.
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UsePostgreSqlStorage(config.GetConnectionString("HangfireConnection")));
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
+        }
+
+        public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Make `Back to site` link working for subfolder applications
+            //var options = new DashboardOptions { AppPath = "~" };
+
+            app.UseHangfireDashboard();
+
+            var backgroundJobs = app.ApplicationServices.GetService<IBackgroundJobClient>();
+            backgroundJobs.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+        }
+    }
+}
