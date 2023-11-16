cc21026a99955fde939941c7046c66e3131f2bf2
liduopie <liduopie@163.com>
2023-11-16 08:48:26 +0800

    更新依赖

--- src/Samples/Leelite.Samples.ModuleSample/ModuleSampleModule.cs
+++ src/Samples/Leelite.Samples.ModuleSample/ModuleSampleModule.cs
@@ -1,16 +1,16 @@
 ﻿using Leelite.Commons.Host;
 using Leelite.Core.Module;
 using Leelite.Samples.ModuleSample.Contexts;
+
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Leelite.Samples.ModuleSample
 {
     public class ModuleSampleModule : ModuleBase
     {
-        public override void ConfigureServices(HostContext context)
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
-            var services = context.ServiceDescriptors;
-
             services.AddDbContext<BloggingContext>("Default");
         }
     }
